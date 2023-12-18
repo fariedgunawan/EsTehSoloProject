aefd18bb78e816f8d8ebf8b082c404bf72577e30
Muhammad Faried Gunawan <fariedmuhammad04@gmail.com>
2023-12-18 10:57:47 +0700

    update ar7 terbaru spasi

--- esTehSoloProject/src/Pages/Detail/Ar7.jsx
+++ esTehSoloProject/src/Pages/Detail/Ar7.jsx
@@ -20,7 +20,7 @@ const Ar7 = () => {
         </h2>
         <h2 className="mt-[20px]">
           Ketika kami wawancarai, salah satu penggemar setia Es Teh S.O.L.O yang sedang mengantre di <span className="italic">booth</span> kami mengungkapkan pendapatnya mengenai Es Teh S.O.L.O. “Dulu aku pikir minuman es teh yang harganya
-          <span className="italic">affordable</span> itu pasti rasanya biasa aja, tapi semua itu berubah ketika aku mencoba Es Teh S.O.L.O. Rasanya tuh <span className="italic">seger</span> dan juga ngangenin!”, kata salah satu penggemar
+          <span className="italic"> affordable</span> itu pasti rasanya biasa aja, tapi semua itu berubah ketika aku mencoba Es Teh S.O.L.O. Rasanya tuh <span className="italic">seger</span> dan juga ngangenin!”, kata salah satu penggemar
           setia Es Teh S.O.L.O. “Setuju, Es Teh S.O.L.O enak dan <span className="italic">seger</span> banget!” ujar pembeli lain yang berada di sebelahnya menimpalkan.
         </h2>
         <div className="img flex flex-col items-center mt-[20px]">
